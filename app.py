4868cbf0fc1c67944a47f1953b3595a73c62f5e8
chsky1600 <ethanchankowsky@gmail.com>
2024-11-21 01:05:42 -0500

    setup app.py for deploy on Heroku

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5002)
+    # Get the port from the environment variable or use a default (e.g., 5000 for local testing)
+    port = int(os.environ.get('PORT', 5002))
+    # Run the app
+    app.run(debug=False, host='0.0.0.0', port=port)
